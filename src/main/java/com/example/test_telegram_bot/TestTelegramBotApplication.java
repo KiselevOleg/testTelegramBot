22f4c4bd0a588758c217bfdc9c9323de6088ad7d
haart <kiselevoleg2016@gmail.com>
2025-03-15 04:16:36 +0300

    code validation has been added and java version has been replaced with 22

---
+++ src/main/java/com/example/test_telegram_bot/TestTelegramBotApplication.java
@@ -0,0 +1,18 @@
+/**
+ * Copyright 2024 Kiselev Oleg
+ */
+package com.example.test_telegram_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Kiselev Oleg
+ */
+@SuppressWarnings({"PMD.UseUtilityClass", "PMD.HideUtilityClassConstructor"})
+@SpringBootApplication
+public class TestTelegramBotApplication {
+    public static void main(final String[] args) {
+        SpringApplication.run(TestTelegramBotApplication.class, args);
+    }
+}
--- src/test/java/com/example/test_telegram_bot/TestTelegramBotApplicationTests.java
+++ src/test/java/com/example/test_telegram_bot/TestTelegramBotApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.testTelegramBot;
+package com.example.test_telegram_bot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class TestTelegramBotApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
